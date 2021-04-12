c9a007d1e4d3c425df7708a09b13219cfbc33b11
Leah King <dancingheart714@yahoo.com>
2021-04-12 17:52:58 -0400

    working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,41 @@
-export const ADD_ONE = "ADD_ONE";
+export const ADD_ONE = 'ADD_ONE';
 
-export const APPLY_NUMBER = "APPLY_NUMBER";
-export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const APPLY_NUMBER = 'APPLY_NUMBER';
+
+export const CHANGE_OPERATION = 'CHANGE_OPERATION';
+
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY';
+
+export const MEMORY_PLUS = 'MEMORY_PLUS';
+
+export const MEMORY_RECALL = 'MEMORY_RECALL';
+
+export const MEMORY_CLEAR = 'MEMORY_CLEAR';
 
 export const addOne = () => {
-    return({type:ADD_ONE});
-}
+  return { type: ADD_ONE };
+};
+
+export const changeOperation = (operator) => {
+  return { type: CHANGE_OPERATION, payload: operator };
+};
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const memoryPlus = () => {
+  return { type: MEMORY_PLUS };
+};
+
+export const memoryRecall = () => {
+  return { type: MEMORY_RECALL };
+};
+
+export const memoryClear = () => {
+  return { type: MEMORY_CLEAR };
+};
